15ef02c9dee02506efcd56b4c42181e331c0fba6
Guna Seelan <113593089+gabeguna579@users.noreply.github.com>
2022-09-24 12:20:25 +0530

    Add files via upload

---
+++ Assignments 2/Assignments/Member 3 Lead (Manikandan)/Assignment 2/python_code.py
@@ -0,0 +1,7 @@
+temperatures=[0,12,17,28,30]
+
+for temperature in temperatures:
+    if temperature>25:
+        print(temperature,'celsius degree is hot')
+    else:
+        print(temperature,'is not hot')
