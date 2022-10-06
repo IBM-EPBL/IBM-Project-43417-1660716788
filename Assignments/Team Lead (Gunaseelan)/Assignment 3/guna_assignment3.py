c9fa3baf63d137ddac561eea3a0643291eae9980
Guna Seelan <113593089+gabeguna579@users.noreply.github.com>
2022-10-06 19:25:53 +0530

    Add files via upload

---
+++ Assignments/Team Lead (Gunaseelan)/Assignment 3/guna_assignment3.py
@@ -0,0 +1,18 @@
+from gpiozero import LED
+from time import sleep
+
+red = LED(22)
+amber = LED(27)
+green = LED(17)
+while True:
+    red.on()
+    sleep(1)
+    red.off()	
+    amber.on()
+    sleep(1)
+    amber.off()
+    green.on()
+    sleep(1)
+    green.off()
+    sleep(1)
+    
